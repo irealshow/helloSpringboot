b43b52bf661df9efc89caed9da4eaab079f2c23a
dingxuan <dingxuan@digitalchina.com>
2017-12-22 16:34:08 +0800

    springboot测试项目

---
+++ src/main/java/com/ding/learn/HelloSpringBoot/StartApplication.java
@@ -0,0 +1,11 @@
+package com.ding.learn.HelloSpringBoot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StartApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(StartApplication.class, args);
+	}
+} 
