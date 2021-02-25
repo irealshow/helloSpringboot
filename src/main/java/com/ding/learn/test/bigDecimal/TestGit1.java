7b2bcbe2dce04a6cb6e6a67da8d45ab6a2e85fd1
01482882 <01482882@yto.net.cn>
2021-02-25 11:08:24 +0800

    Revert "localdate提交"
    This reverts commit 8f4ddd77fee2be23e8a6a3319627c3741b15864f.

--- src/main/java/com/ding/learn/test/bigDecimal/TestGit1.java
+++ src/main/java/com/ding/learn/test/bigDecimal/TestGit1.java
@@ -3,5 +3,6 @@ package com.ding.learn.test.bigDecimal;
 public class TestGit1 {
     public static void main(String[] args) {
         System.out.println("test one");
+        System.out.println("test two");
     }
 }
