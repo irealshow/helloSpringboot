1d0d5c7caec45c3f900765bc42da209d307b730f
01482882 <01482882@yto.net.cn>
2021-02-25 11:14:06 +0800

    add test three

--- src/main/java/com/ding/learn/test/bigDecimal/TestGit1.java
+++ src/main/java/com/ding/learn/test/bigDecimal/TestGit1.java
@@ -4,5 +4,6 @@ public class TestGit1 {
     public static void main(String[] args) {
         System.out.println("test one");
         System.out.println("test two");
+        System.out.println("test three");
     }
 }
