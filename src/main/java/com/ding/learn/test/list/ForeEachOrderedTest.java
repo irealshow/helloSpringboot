6339e67859ac4de5632a04a5443afbe4a7503ed3
01482882 <01482882@yto.net.cn>
2021-03-05 11:15:04 +0800

    并行流捕获异常的问题处理

--- src/main/java/com/ding/learn/test/list/ForeEachOrderedTest.java
+++ src/main/java/com/ding/learn/test/list/ForeEachOrderedTest.java
@@ -4,7 +4,24 @@ import java.util.stream.IntStream;
 import java.util.stream.Stream;
 
 public class ForeEachOrderedTest {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
+        try {
+//            IntStream.range(-5,5).forEach(n-> System.out.println(n));
+            IntStream.range(-5,10).parallel().forEach(n->{
+                try {
+                    int result = 10/n;
+                    System.out.println(result+":"+Thread.currentThread().getName());
+                } catch (Exception e) {
+                    throw new RuntimeException("并行流执行出错");
+                }
+            });
+        } catch (Exception e) {
+            System.out.println(e.getMessage()+Thread.currentThread().getName());
+        }
+    }
+
+
+    public static void main1(String[] args) {
         //forEach 与 foreEachOrdered 的区别 主要的区别在并行流的处理上
        /* Stream.of("A","B","C","D","E","F","G").parallel().forEach(System.out::print);
         System.out.println("");
